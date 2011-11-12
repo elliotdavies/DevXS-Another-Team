6add81458762aadcf27d3139234d53d7a6c0e223
Sam Elliott <sam@lenary.co.uk>
2011-11-12 15:59:46 +0000

    munging

--- hesa_data_subject_area_by_level_munging.rb
+++ hesa_data_subject_area_by_level_munging.rb
@@ -6,12 +6,30 @@ require 'pp'
 
 $db = CouchRest.database!("http://127.0.0.1:5984/hesa_subject_areas")
 
-$db.all_docs(:include_docs => true)["rows"].each do |mr_d|
-  id = mr_d["id"]
-  doc = mr_d["doc"]
-  rev = doc["_rev"]
+under_vals = []
+post_vals = []
+other_vals = []
 
-  pp doc
+$db.all_docs(:include_docs => true)["rows"].each do |mr_d|
+  doc = mr_d["doc"]
+  under_vals << doc["undergraduate"]
+  post_vals << doc["postgraduate"]
+  other_vals << doc["other"]
 end
+
+under_vals.sort!.reverse!
+post_vals.sort!.reverse!
+other_vals.sort!.reverse!
+
+$db.all_docs(:include_docs => true)["rows"].each do |mr_d|
+  doc = mr_d["doc"]
+  doc["undergraduate_rank"] = under_vals.index(doc["undergraduate"]) + 1
+  doc["postgraduate_rank"] = post_vals.index(doc["postgraduate"]) + 1
+  doc["other_rank"] = other_vals.index(doc["other"]) + 1
+
+  $db.save_doc(doc)
+end
+
+
 
 
